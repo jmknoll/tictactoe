3bb316ef4e5cf4a3759d9ed32c40bc77d58ca251
Jameson M. Knoll <j.knoll621@gmail.com>
2014-07-01 13:18:44 +0800

    added functional jquery - still need game logic

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+$(document).ready(function(){
 
-var $board = $('<div class="gameBoard"></div>')
-var $box = $('<div class="gameBox"></div>')
+//var $board = $('<div class="gameBoard"></div>')
+var $gameBox = "<div class='gameBox'></div>"
 
-$("document").ready(function() {
-	for (i = 0; i < 9; i++){
-		$board.append($box) 
+for (x=0; x<9; x++){
+	$(".gameBoard").append($gameBox); 
 		//build game board
 		}
-	});
 
+var imageCounter = 0
+
+	$('.gameBox').click(function(){
+
+		$(this).css('background-image','url(images/x.png)');
+	});
 // onClick change the color of the box
 // change to an x or an o depending on even/odd of turn counter
 // then build logic for computer's decision making process
 	// start with random
 	// then improve to choices based on where human player went
 
+});
