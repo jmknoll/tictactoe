5698bc29707504dfe841d62792fa83eebbc42dce
Jameson M. Knoll <j.knoll621@gmail.com>
2014-08-09 01:38:29 +0800

    minor GUI changes

--- script.js
+++ script.js
@@ -10,24 +10,35 @@ for (x=0; x<9; x++){
 
 var imageCounter = 0
 
+numPlayers = prompt("1 Player or 2?", "2")
 
 //this is the code for the two-player version
-//comment out with a docstring to run the ai version
+if (numPlayers == 2){
 
-$('.gameBox').click(function(){
+	$('.gameBox').click(function(){
 
-	if (imageCounter % 2 == 0){
+		if (imageCounter % 2 == 0){
+
+			$(this).css('background-image','url(images/x.png)');
+			imageCounter = imageCounter + 1;
+
+		} else {
+		
+			$(this).css('background-image','url(images/o.png)');
+			imageCounter = imageCounter + 1;
+
+			}
+		});
+
+} else {
+	$('.gameBox').click(function(){
 
 		$(this).css('background-image','url(images/x.png)');
 		imageCounter = imageCounter + 1;
 
-	} else {
-	
-		$(this).css('background-image','url(images/o.png)');
-		imageCounter = imageCounter + 1;
+		});
 
-		}
-	});
+}
 
 //this is the code for the ai version
 //comment out with a docstring to run the two-player version
